9a631b3f141fae852a53b6eee780a13db00c26aa
Gabriel Aquino <gabriel.aquino@contraclabs.tech>
2024-05-26 21:22:36 -0300

    feat: added create and handler for error

--- lib/bank_web/controllers/AccountsController.ex
+++ lib/bank_web/controllers/AccountsController.ex
@@ -1,13 +1,56 @@
 defmodule BankWeb.AccountsController do
-  @moduledoc false
+  @moduledoc """
+  Controller for managing bank accounts.
+  """
 
   use BankWeb, :controller
 
   alias Bank.Account.Context
+  alias BankWeb.ErrorHandler
 
-  def list(conn, params) do
-    accounts = Context.all(params)
+  @doc """
+  Creates a new account.
 
-    json(conn, %{data: accounts})
+  ## Parameters
+
+  - `conn`: The connection struct.
+  - `params`: A map containing the parameters for creating the account.
+    - `account_number`: The account number.
+    - `balance`: The account balance.
+
+  """
+  def create(conn, %{"account_number" => account_number, "balance" => balance}) do
+    try do
+      account_params = %{account_number: account_number, balance: balance}
+      {:ok, account} = Context.create(account_params)
+
+      conn
+      |> put_status(:created)
+      |> json(%{account_number: account.account_number, balance: account.balance})
+    rescue
+      error -> handle_error(conn, error)
+    end
+  end
+
+  @doc """
+  Lists all accounts.
+
+  ## Parameters
+
+  - `conn`: The connection struct.
+  - `params`: Ignored in this method.
+
+  """
+  def list(conn, _params) do
+    try do
+      accounts = Context.all([])
+      json(conn, %{accounts: accounts})
+    rescue
+      error -> handle_error(conn, error)
+    end
+  end
+
+  defp handle_error(conn, error) do
+    ErrorHandler.handle_errors(conn, error)
   end
 end
